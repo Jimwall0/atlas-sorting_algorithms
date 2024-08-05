a598926d6bfc7d4df1e8390210bb8e6fd96af988
Tsimmuaj <tyang4204@gmail.com>
2024-08-05 15:52:29 -0500

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array
+ * @array: the array to sort
+ * @size: the limit of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	long unsigned int loop, subtract;
+	int ptr;
+
+	for (subtract = 0; subtract < size; subtract++)
+	{
+		for (loop = 0; loop < size - subtract; loop++)
+		{
+			ptr = array[loop - 1];
+			if (ptr > array[loop])
+			{
+				array[loop - 1] = array[loop];
+				array[loop] = ptr;
+				print_array(array, size);
+			}
+		}
+	}
+}
